8803ae1c9400565aa09793b021480f3032947f32
cetrinw <cetrinw@sina.com>
2016-04-21 12:08:30 +0800

    step3

--- src/main/java/com/cetrinw/liu/learn/Step2.java
+++ src/main/java/com/cetrinw/liu/learn/Step2.java
@@ -3,8 +3,20 @@ package com.cetrinw.liu.learn;
 /**
  * Created by Cetrin Wang on 2016/4/21.
  * Session 2
- * 写一个带参方法,传入一个String类型文本,并在控制太打印
+ * 写一个带参方法,传入一个String类型文本,并在main方法打印
  */
 public class Step2 {
 
+    /**
+     * 对输入文本进行打印
+     * @param str 需要打印的内容
+     */
+    private static void print(String str){
+        System.out.println(str);
+    }
+
+    public static void main(String[] args) {
+        print("Hello World!");
+        print("Hello Liu Chen!");
+    }
 }
