3ba969e38abdd3adef0d572eecc467cba671553c
supce <949397097@qq.com>
2016-04-21 10:47:17 +0800

    chushi daoru

---
+++ src/main/java/com/cetrinw/liu/learn/Step1.java
@@ -0,0 +1,12 @@
+package com.cetrinw.liu.learn;
+
+/**
+ * Created by Dell-pc on 2016/4/21.
+ * Session1
+ * 输出hello world
+ */
+public class Step1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
