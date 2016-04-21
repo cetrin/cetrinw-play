75749bee49e04e40af3f8be64e3ba4b86358ead7
supce <949397097@qq.com>
2016-04-21 12:41:08 +0800

    chushi daoru

--- src/main/java/com/cetrinw/liu/learn/Step4.java
+++ src/main/java/com/cetrinw/liu/learn/Step4.java
@@ -6,4 +6,25 @@ package com.cetrinw.liu.learn;
  * 实现基本的加减乘除
  */
 public class Step4 {
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static int sub(int a,int b){
+        return a-b;
+    }
+    public static int mul(int a,int b){
+        return a*b;
+    }
+    public static int div(int a,int b){
+        return a/b;
+    }
+
+    public static void main(String[] args) {
+        int a=63;
+        int b=12;
+        System.out.println(add(a,b));
+        System.out.println(sub(a,b));
+        System.out.println(mul(a,b));
+        System.out.println(div(a,b));
+    }
 }
