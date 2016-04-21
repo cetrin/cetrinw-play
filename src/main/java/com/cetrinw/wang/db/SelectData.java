e8bcc80f1fffc26ea24af2fa89e7ad0f3018cef6
cetrinw <cetrinw@sina.com>
2016-04-21 15:22:39 +0800

    step3

--- src/main/java/com/cetrinw/wang/db/SelectData.java
+++ src/main/java/com/cetrinw/wang/db/SelectData.java
@@ -1,7 +1,32 @@
 package com.cetrinw.wang.db;
 
+import com.cetrinw.util.DatabaseUtil;
+import com.cetrinw.wang.entity.Cetrinw;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.List;
+
 /**
  * Created by Cetrin Wang on 2016/4/21.
  */
 public class SelectData {
+
+    private MongoTemplate template;
+
+    private SelectData(){
+        template = new MongoTemplate(DatabaseUtil.getMongodbConnection(),"local");
+    }
+
+    private void getData(){
+        List<Cetrinw>  list =  template.findAll(Cetrinw.class);
+        for (Cetrinw content : list) {
+            System.out.println(content.getContent());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SelectData s = new SelectData();
+        s.getData();
+    }
 }
---
+++ src/main/java/com/cetrinw/wang/entity/Cetrinw.java
@@ -0,0 +1,25 @@
+package com.cetrinw.wang.entity;
+
+/**
+ * Created by Cetrin Wang on 2016/4/21.
+ */
+public class Cetrinw {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
