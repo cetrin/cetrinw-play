ef36de7b7e39d10a81755bb6b273e204506332be
cetrinw <cetrinw@sina.com>
2016-04-21 14:32:33 +0800

    step3

---
+++ src/test/com/cetrinw/wang/db/DataMigrateTest.java
@@ -0,0 +1,14 @@
+package com.cetrinw.wang.db;
+
+import org.junit.Test;
+
+/**
+ * Created by Cetrin Wang on 2016/4/21.
+ */
+public class DataMigrateTest {
+    @Test
+    public void saveDataToMongodb() throws Exception {
+
+    }
+
+}
